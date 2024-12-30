b3ec3018005d3635003c0f2591ed1971b4a1f58e
Jannis Metrikat <120120832+jmetrikat@users.noreply.github.com>
2024-12-30 14:22:27 +0100

    Fix request spamming

--- src/browse-spaces.tsx
+++ src/browse-spaces.tsx
@@ -1,5 +1,5 @@
 import { List, Image } from "@raycast/api";
-import { useState, useEffect, useMemo } from "react";
+import { useState, useEffect, useMemo, useRef } from "react";
 import SpaceListItem from "./components/SpaceListItem";
 import { useSpaces } from "./hooks/useSpaces";
 import { getMembers } from "./api/getMembers";
@@ -8,35 +8,40 @@ import { pluralize } from "./utils/helpers";
 export default function BrowseSpaces() {
   const { spaces, isLoadingSpaces } = useSpaces();
   const [searchText, setSearchText] = useState("");
-  const [membersData, setMembersData] = useState<{ [key: string]: number }>({});
+  const membersDataRef = useRef<{ [key: string]: number }>({});
 
   useEffect(() => {
     if (spaces) {
       const fetchMembersData = async () => {
         const data: { [key: string]: number } = {};
-        for (const space of spaces) {
-          try {
-            const response = await getMembers(space.id, {
-              offset: 0,
-              limit: 1,
-            });
-            data[space.id] = response.pagination.total;
-          } catch (error) {
-            console.error(
-              `Failed to fetch members for space ${space.id}:`,
-              error,
-            );
-          }
-        }
-        setMembersData(data);
+        const spaceIds = spaces.map((space) => space.id);
+        const uniqueSpaceIds = spaceIds.filter(
+          (id) => !(id in membersDataRef.current),
+        );
+
+        await Promise.all(
+          uniqueSpaceIds.map(async (id) => {
+            try {
+              const response = await getMembers(id, {
+                offset: 0,
+                limit: 1,
+              });
+              data[id] = response.pagination.total;
+            } catch (error) {
+              console.error(`Failed to fetch members for space ${id}:`, error);
+            }
+          }),
+        );
+
+        membersDataRef.current = { ...membersDataRef.current, ...data };
       };
       fetchMembersData();
     }
   }, [spaces]);
 
   const isLoadingMembers = useMemo(
-    () => Object.keys(membersData).length !== spaces?.length,
-    [membersData, spaces],
+    () => Object.keys(membersDataRef.current).length !== spaces?.length,
+    [membersDataRef.current, spaces],
   );
 
   const filteredSpaces = spaces?.filter((space) =>
@@ -56,7 +61,7 @@ export default function BrowseSpaces() {
         })}
       >
         {filteredSpaces?.map((space) => {
-          const memberCount = membersData[space.id] || 0;
+          const memberCount = membersDataRef.current[space.id] || 0;
           return (
             <SpaceListItem
               space={space}
