d4361cf4d702030dbdc822fd739bf6d9409e88e9
Jannis Metrikat <120120832+jmetrikat@users.noreply.github.com>
2025-05-26 12:40:25 +0200

    Add v4 changelog

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -4,7 +4,7 @@ import { BodyFormat } from "../models";
 import { encodeQueryParams } from "./query";
 
 // Strings
-export const apiAppName = "raycast_v3_0425";
+export const apiAppName = "raycast_v4_0525";
 export const anytypeNetwork = "N83gJpVd9MuNRZAuJLZ7LiMntTThhPc6DtzWWVjb1M3PouVU";
 export const errorConnectionMessage = "Can't connect to API. Please ensure Anytype is running and reachable.";
 
