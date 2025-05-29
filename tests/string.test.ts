527710d2d974ae57cd170f63a167cc670ac67cfb
Jannis Metrikat <120120832+jmetrikat@users.noreply.github.com>
2025-05-29 23:35:30 +0200

    Remove string test

--- tests/string.test.ts
+++
@@ -1,9 +0,0 @@
-import assert from 'assert';
-import { pluralize } from '../src/utils/string';
-
-assert.strictEqual(pluralize(2, 'city'), 'cities');
-assert.strictEqual(pluralize(2, 'key'), 'keys');
-assert.strictEqual(pluralize(2, 'boy'), 'boys');
-assert.strictEqual(pluralize(1, 'city'), 'city');
-
-console.log('All tests passed');
